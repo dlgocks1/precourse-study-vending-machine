59aa207bbe0ade0740e6037b044bdc7c0407db8f
dlgocks1 <cksgodl1@naver.com>
2022-12-16 03:37:40 +0900

    feat : 상품 구매, 매진 로직 구현

--- src/main/kotlin/vendingmachine/domain/model/Product.kt
+++ src/main/kotlin/vendingmachine/domain/model/Product.kt
@@ -3,5 +3,13 @@ package vendingmachine.domain.model
 data class Product(
     val name: String,
     val prize: Int,
-    val count: Int,
-)
+    var count: Int,
+) {
+    fun isSoldOut(): Boolean {
+        return count == 0
+    }
+
+    fun purchase() {
+        count -= 1
+    }
+}
--- src/main/kotlin/vendingmachine/domain/model/Products.kt
+++ src/main/kotlin/vendingmachine/domain/model/Products.kt
@@ -1,6 +1,20 @@
 package vendingmachine.domain.model
 
 class Products(private val products: List<Product>) : List<Product> by products {
+    fun minPrize(): Int = products.minOf { it.prize }
+    fun isSoldOut(): Boolean = products.all { it.count == 0 }
+    fun purchase(product: String) {
+        checkValidProduct(product)
+        products.find { it.name == product }?.purchase()
+    }
+
+    private fun checkValidProduct(product: String) {
+        val isSoldOut = products.find { it.name == product }?.isSoldOut()
+            ?: throw IllegalArgumentException("구매할 상품이 존재하지 않습니다.")
+        require(!isSoldOut) {
+            "구매할 상품이 매진 상태입니다."
+        }
+    }
 
     companion object {
         fun valueOf(products: String): Products {
