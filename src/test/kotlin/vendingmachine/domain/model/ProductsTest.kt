386f7eedb7624962176264703b01914e29d908f1
dlgocks1 <cksgodl1@naver.com>
2022-12-16 03:37:14 +0900

    test : 상품 구매 과정 테스트 추가
    * 상품들의 최소가격 반환 테스트
    * 상품 구매할 때 재고 감소 테스트
    * 상품을 구매할 때 매진이면 에러 발생 테스트
    * 상품을 구매할 때 없으면 에러 발생 테스트

--- src/test/kotlin/vendingmachine/domain/model/ProductsTest.kt
+++ src/test/kotlin/vendingmachine/domain/model/ProductsTest.kt
@@ -7,9 +7,49 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 class ProductsTest {
+
+    @Test
+    @DisplayName("상품을 구매하려 했을 때 해당 상품이 없으면 에러를 발생한다.")
+    fun invalidProductNameTest() {
+        val proudctInput = "[콜라,1500,0];[사이다,1000,10]"
+        assertThrows<IllegalArgumentException> {
+            Products.valueOf(proudctInput)
+                .purchase("솔의눈")
+        }
+    }
+
+    @Test
+    @DisplayName("상품을 구매하려 했을 때 해당 상품이 매진이면 에러를 발생한다.")
+    fun soldOutTest() {
+        val proudctInput = "[콜라,1500,0];[사이다,1000,10]"
+        assertThrows<IllegalArgumentException> {
+            Products.valueOf(proudctInput)
+                .purchase("콜라")
+        }
+    }
+
+    @Test
+    @DisplayName("상품을 구매할 때 재고가 감소하는지 테스트한다.")
+    fun purchaseTest() {
+        val proudctInput = "[콜라,1500,10];[사이다,1000,10]"
+        assertThat(
+            Products.valueOf(proudctInput).apply {
+                purchase("콜라")
+            }
+        ).contains(Product("콜라", 1500, 9))
+    }
+
+    @Test
+    @DisplayName("상품의 최소가격을 반환하는지 테스트한다.")
+    fun minPrizeTest() {
+        val proudctInput = "[콜라,1500,10];[사이다,1000,10]"
+        assertThat(
+            Products.valueOf(proudctInput).minPrize()
+        ).isEqualTo(1000)
+    }
 
     @Test
     @DisplayName("올바른 상품을 파싱하여 객체로 반환하는지 테스트한다.")
