409c03641c2cd63b0830020ff55b9119800e2bfc
dlgocks1 <cksgodl1@naver.com>
2022-12-16 00:28:35 +0900

    feat : InputView 작성
    지원하는 기능
    * 자판기 금액 입력
    * 자판기 상품 입력
    * 유저 금액 입력

---
+++ src/main/kotlin/vendingmachine/view/InputView.kt
@@ -0,0 +1,22 @@
+package vendingmachine.view
+
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
+class InputView {
+
+    fun vendingMachineAmount(): Int {
+        println("자판기가 보유하고 있는 금액을 입력해 주세요.")
+        return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("금액은 숫자로 입력해야 합니다.")
+    }
+
+    fun vendingMachinProduct(): String {
+        println("상품명과 가격, 수량을 입력해 주세요.")
+        return Console.readLine()
+    }
+
+    fun userAmount(): Int {
+        println("투입 금액을 입력해 주세요.")
+        return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("금액은 숫자로 입력해야 합니다.")
+    }
+}
