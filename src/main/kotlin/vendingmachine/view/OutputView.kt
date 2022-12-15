ba2d5a24d5553be3f645909bddc8b25a9d14085c
dlgocks1 <cksgodl1@naver.com>
2022-12-16 00:34:29 +0900

    feat : OutputView 구조 작성

---
+++ src/main/kotlin/vendingmachine/view/OutputView.kt
@@ -0,0 +1,20 @@
+package vendingmachine.view
+
+class OutputView {
+
+    /**
+     * 자판기가 보유한 동전을 출력한다.
+     */
+    fun vendingMachinCoin() {
+        println()
+    }
+
+    /**
+     * 잔돈을 출력한다.
+     */
+    fun result() {
+        println("투입 금액: 0")
+        println("잔돈")
+        // 잔돈 출력
+    }
+}
