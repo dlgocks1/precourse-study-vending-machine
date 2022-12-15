f0f13fb6c29b46a2dd2a27346c38452ea977b27b
dlgocks1 <cksgodl1@naver.com>
2022-12-16 00:34:43 +0900

    feat : InputView 구매 상품 함수 추가

--- src/main/kotlin/vendingmachine/view/InputView.kt
+++ src/main/kotlin/vendingmachine/view/InputView.kt
@@ -15,6 +15,12 @@ class InputView {
         return Console.readLine()
     }
 
+    fun purchaseProduct(amount: Int): String {
+        println("투입 금액: ${amount}원")
+        println("구매할 상품명을 입력해 주세요.")
+        return Console.readLine()
+    }
+
     fun userAmount(): Int {
         println("투입 금액을 입력해 주세요.")
         return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("금액은 숫자로 입력해야 합니다.")
