0482acc2c5f243962c9fbaf889c4c61a924b791e
dlgocks1 <cksgodl1@naver.com>
2022-12-16 01:29:55 +0900

    feat : Coins클래스가 단위 검증 추가

--- src/main/kotlin/vendingmachine/domain/model/Coins.kt
+++ src/main/kotlin/vendingmachine/domain/model/Coins.kt
@@ -5,6 +5,9 @@ class Coins {
     private var coins: List<Coin> = listOf(Coin.COIN_500, Coin.COIN_100, Coin.COIN_50, Coin.COIN_10)
 
     fun slotCoin(coin: Int) {
+        require(validateCoinUnit(coin)) {
+            "동전의 단위가 잘 못 입력됬습니다."
+        }
         when (Coin.createCoin(coin)) {
             Coin.COIN_500 -> coins[0].addCount()
             Coin.COIN_100 -> coins[1].addCount()
@@ -12,6 +15,8 @@ class Coins {
             Coin.COIN_10 -> coins[3].addCount()
         }
     }
+
+    private fun validateCoinUnit(unit: Int) = unitList.contains(unit)
 
     /**
      * 코인리스트를 반환한다.
@@ -21,5 +26,9 @@ class Coins {
         it.count
     }
 
+    companion object {
+        val unitList = listOf(500, 100, 50, 10)
+
+    }
 
 }
